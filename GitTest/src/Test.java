9cd4b7b59109519b5db1a6227336896fcacb565a
박서현 <90893579+pkwesst@users.noreply.github.com>
2021-12-02 15:34:40 +0900

    Update Test.java

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -4,8 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("First Code!");
 		System.out.println("psh test!");
-		System.out.println("github~")
-		System.out.println("god")
+		System.out.println("github~");
+		System.out.println("god");
 
 	}
 
