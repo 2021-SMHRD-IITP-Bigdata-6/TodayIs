6abf4785e5d23fadcc520cf6f5507f37688ef13b
smhrd <smhrd@DESKTOP-Q9DQAOQ>
2021-12-28 16:28:32 +0900

    작업중

---
+++ src/com/today/member/CommDelService.java
@@ -0,0 +1,33 @@
+package com.today.member;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.today.DAO.commDAO;
+
+import javafx.scene.control.Alert;
+
+
+@WebServlet("/CommDelService")
+public class CommDelService extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+
+		String COMM_SEQ = request.getParameter("COMM_SEQ");
+		
+		commDAO dao = new commDAO();
+		System.out.println(COMM_SEQ);
+		int cnt = dao.comm_delete(COMM_SEQ);
+		
+		if (cnt > 0) {
+			response.sendRedirect("t_community_board.jsp");
+		} else {
+			response.sendRedirect("t_community_board.jsp");
+		}
+	}
+
+}
